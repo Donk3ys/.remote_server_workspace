802d59eccb99699247263b1f68cbd96acc575bb6
donk3ys <davidtgericke@gmail.com>
2022-08-22 11:05:40 +0200

    feat: remove stale files

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -19,6 +19,7 @@ cd ~
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+sudo rm lazygit.tar.gz
 lazygit --version
 
 # Install "Packer" plugin manager for neovim
@@ -28,6 +29,7 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 # Install neovim
 wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
 
 # Add nvim as alias of vim
 echo "alias vim='/home/ubuntu/nvim-linux64/bin/nvim'" >> .bashrc
